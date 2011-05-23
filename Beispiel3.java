f5c70a40ee136d91e378a6070124b64f49730f5d
David Mihola <david.mihola@gmx.net>
2011-05-23 16:37:05 +0200

    dom() kopiert jetzt auch die Datei - nur eben nach der DOM-Methode

--- Beispiel3.java
+++ Beispiel3.java
@@ -3,6 +3,10 @@
  * Uebungsbeispiel 3
  */
 
+import api.*;
+
+import java.io.File;
+
 import org.xml.sax.SAXException;
 
 import org.xml.sax.XMLReader;
@@ -15,10 +19,18 @@ import org.xml.sax.Attributes;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 
+import javax.xml.transform.dom.DOMSource;
+
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.xml.sax.helpers.XMLFilterImpl;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+
+// org.xml.sax.SAXParseException;
 
 
 public class Beispiel3 {
@@ -137,5 +149,30 @@ public class Beispiel3 {
      */
     private void dom(String filteredMemoryXML, String checkedMemoryXML)
             throws Exception {
+
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+	//factory.setValidating(true);
+	factory.setNamespaceAware(true);
+	factory.setIgnoringComments(true);
+	factory.setIgnoringElementContentWhitespace(true);
+
+	DocumentBuilder builder = factory.newDocumentBuilder();
+
+	Document document = builder.parse(new File(filteredMemoryXML));
+
+	Game game = new Game(document);
+
+	// hier muss man jetzt noch aus dem game die Paare auslesen und in das
+	// DOM einbauen
+
+	DOMSource source = new DOMSource(document);
+	StreamResult result = new StreamResult(checkedMemoryXML);
+
+	TransformerFactory transFactory = TransformerFactory.newInstance();
+	Transformer transformer = transFactory.newTransformer();
+
+	transformer.transform(source, result);
+
 	}
 }
